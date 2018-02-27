b71ea4eeb26eb008557999579e8c13f83da74423
Jakub Lemka <lemka.jakub@gmail.com>
2018-02-27 17:02:39 +0100

    styling and first scripts

---
+++ js/app.js
@@ -0,0 +1,63 @@
+class Budget {
+  constructor(budget) {
+    this.budget = Number(budget);
+    this.budgetLeft = budget;
+  }
+}
+
+class HTML {
+  insertBudget(amount) {
+    budgetTotal.innerHTML = amount;
+    budgetLeft.innerHTML = amount;
+  }
+
+  printMessage(message, className){
+    const messageWrapper = document.createElement('div');
+    messageWrapper.classList.add('class1', 'class2', className);
+    messageWrapper.appendChild(document.createTextNode(message));
+
+    document.getElementById('adding').insertBefore(messageWrapper, addExpenseForm);
+
+    setTimeout(function(){
+      messageWrapper
+    }, 3000);
+  }
+}
+
+
+const addExpenseForm = document.getElementById('add-expense');
+const budgetTotal = document.getElementById('total');
+const budgetLeft = document.getElementById('left');
+
+let budget, userBudget;
+
+const html = new HTML();
+
+eventListeners();
+
+function eventListeners() {
+
+  document.addEventListener('DOMContentLoaded', function() {
+    userBudget = prompt(' Input your weekly budget ');
+
+    if (userBudget === null || userBudget === '' || userBudget === '0') {
+      window.location.reload();
+    } else {
+      budget = new Budget(userBudget);
+
+      html.insertBudget(budget.budget);
+    }
+  });
+
+  addExpenseForm.addEventListener('submit', function(e) {
+    //e.preventDefault();
+    const expenseName = document.getElementById('expense').value;
+    const amount = document.getElementById('amount').value;
+
+    if (expenseName === '' || amount === ''){
+      html.printMessage('All fields are mandatory', 'error');
+    } else {
+      console.log('OK');
+    }
+  });
+}
